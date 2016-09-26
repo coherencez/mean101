a31f39feafede8ccfd4fcced989cb23fc9a7ee6c
Thomas Swann <tkswann@mac.com>
2016-09-26 09:32:52 -0500

    inital boilerplate code

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express')
   ,       app = express()
   ,      port = process.env.PORT || 3000
 
-app.use(express.static('?'))
+app.use(express.static('client'))
 
 app.listen(port, () => {
   console.log(`Now listening on port ${port}`)
