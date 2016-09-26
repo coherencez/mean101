06cc143360bb0b6a3414a04d8ab0583be754b180
Thomas Swann <tkswann@mac.com>
2016-09-26 09:26:41 -0500

    minimal express static server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const express = require('express')
+  ,       app = express()
+  ,      port = process.env.PORT || 3000
+
+app.use(express.static('?'))
+
+app.listen(port, () => {
+  console.log(`Now listening on port ${port}`)
+})
